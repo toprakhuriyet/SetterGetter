db5a26f485ae21c56ef3b1ce549c5782de55aa91
toprakhuriyet <62390533+toprakhuriyet@users.noreply.github.com>
2022-05-25 14:50:45 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Toprak
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Car car1 = new Car("Gümüş", 1.5, "Renault Megane", "Diesel", 555000);
+        car1.showInfos();
+        
+        
+        
+        
+        
+        
+        
+       
+                                       
+
+
+
+    }
+    
+}
